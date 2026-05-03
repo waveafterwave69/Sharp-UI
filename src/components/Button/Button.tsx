45344242a4039292bab9c1687f7125bd4564113a
Michael Shonov <waveafterwave3w2@gmail.com>
2026-05-03 15:02:19 +0300

    fix(Button): ts button error; onClick prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,26 @@
-import type { FC, ReactNode } from 'react'
+import type { FC, ReactNode, ButtonHTMLAttributes } from 'react'
 import styles from './Button.module.css'
 import { classNames } from '../../helpers/helpers'
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     appearance?: 'primary' | 'secondary'
     size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
     children?: ReactNode
-    className: string
+    className?: string
+    onClick?: () => void
 }
 
 const Button: FC<ButtonProps> = ({
     appearance = 'primary',
-    size = 'm',
+    size = 'md',
     children,
     className = '',
+    onClick,
     ...props
 }) => {
     return (
         <button
+            onClick={onClick}
             className={classNames(
                 styles.button,
                 {
